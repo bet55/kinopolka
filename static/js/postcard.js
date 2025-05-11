b992dc522c39dc5bae4785eb55744bdf6dd14b28
Kazantcev Stepan <stepanda96@yandex.ru>
2025-05-11 20:50:42 +0500

    Подтверждение отправки приглашения.

--- static/js/postcard.js
+++ static/js/postcard.js
@@ -1,10 +1,11 @@
 import {cartHandler} from "./postcard/movies_cart.js";
 import {savePostcard} from "./postcard/save_postcard.js";
 import {createPostcard} from "./postcard/create_postcard.js";
-import {sendPostcard} from "./postcard/send_postcard_invitation.js";
+import {confirmSaving} from "./postcard/send_postcard_invitation.js";
+
 
 
 cartHandler(); // обработка работы с корзиной
 await savePostcard(); // сохранение открытки
 await createPostcard(); // создание новой открытки
-await sendPostcard(); // отправка открытки пользователям
+await confirmSaving(); // отправка открытки пользователям
--- static/js/postcard/send_postcard_invitation.js
+++ static/js/postcard/send_postcard_invitation.js
@@ -1,35 +1,69 @@
 import {Request} from "../utils/request.js";
 
+// Кнопка для отправки приглашения
+const sendButton = document.querySelector('#postcard-send-button');
+
 // Отправляем приглашение с открыткой (email, telegram)
+async function sendPostcard() {
 
-export async function sendPostcard() {
+    // Отображаем выполнение запроса
+    sendButton.classList.add('active_option');
 
-    const sendButton = document.querySelector('#postcard-send-button');
+    // Запрос на отправку открытки
+    const url = '/invitation/';
+    // await Request.send({method: 'post', url: url});
+    await new Promise(resolve => setTimeout(resolve, 3000));
 
+    sendButton.classList.remove('active_option');
+}
+
+
+// Запрашиваем подтверждение на отправку приглашения
+export async function confirmSaving() {
+
+    // Не нашли кнопку
     if (!sendButton) {
         return null;
     }
 
+    // Контейнер с подтверждением отправления
+    const confirmContainer = document.querySelector('.confirmation');
+    const confirmYes = confirmContainer.querySelector('#confirm-yes');
+    const confirmNo = confirmContainer.querySelector('#confirm-no');
+
+    // Флаг выполнения запроса
     let isPending = false;
 
-    sendButton.addEventListener('click', async () => {
 
-        // Всё еще исполняется предыдущее действие
+    // Отрисовка формы подтверждения
+    sendButton.addEventListener('click', () => {
         if (isPending) {
             return null;
         }
-
-        // Отображаем выполнение запроса
-        isPending = true;
-        sendButton.classList.add('active_option');
-
-        // Запрос на отправку открытки
-        const url = '/invitation/';
-        await Request.send({method: 'post', url:url});
-
-        // Отображаем выполнение запроса
-        isPending = false;
-        sendButton.classList.remove('active_option');
-
+        confirmContainer.classList.toggle('active');
     })
+
+
+    // Отправка
+    confirmYes.addEventListener('click', async () => {
+
+        if (isPending) {
+            return null;
+        }
+        confirmContainer.classList.remove('active');
+
+        isPending = true;
+
+        await sendPostcard(isPending);
+
+        isPending = false;
+    })
+
+    // Отмена отправки
+    confirmNo.addEventListener('click', () => {
+        confirmContainer.classList.remove('active');
+    })
+
 }
+
+
